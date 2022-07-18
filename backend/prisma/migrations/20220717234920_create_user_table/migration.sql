2507006d87656922510d1f85804f23a28b584e15
Thiago <thiago.poison@gmail.com>
2022-07-17 21:59:13 -0300

    backend initial commit

---
+++ backend/prisma/migrations/20220717234920_create_user_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
---
+++ backend/src/server.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import { PrismaClient, User } from '@prisma/client';
+import cors from 'cors';
+import { compare, hash } from 'bcryptjs';
+
+const prisma = new PrismaClient();
+const server = express();
+server.use(cors());
+server.use(express.json());
+// server.get('/product', async (request, response) => {
+//   const products = await prisma.product.findMany();
+//   console.log(products);
+//   return response.json(products);
+// });
+server.post('/register', async (request, response) => {
+  const { email, name, password } = request.body as User;
+  const hashedPassword = await hash(password, 5);
+  const user = await prisma.user.create({ data: { email, name, password: hashedPassword } });
+  return response.status(201).json(user);
+});
+
+server.post('/login', async (request, response) => {
+  const { email, password } = request.body as User;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return response.status(401).json({ message: 'Wrong email or password.' });
+  }
+  const validPassword = await compare(password, user.password);
+  if (!validPassword) {
+    return response.status(401).json({ message: 'Wrong email or password.' });
+  }
+  return response.json({ token: '' });
+});
+
+// server.delete('/product/:id', async (request, response) => {
+//   const { id } = request.params;
+//   await prisma.product.delete({ where: { id: parseInt(id, 10) } });
+//   return response.status(204).json();
+// });
+
+server.listen(3333, () => {
+  console.log('Agora foi');
+});
